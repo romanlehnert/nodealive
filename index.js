8bda62aa2dbdaf27843b5007a2327afd451f924d
Roman <roman.lehnert@googlemail.com>
2012-08-08 17:24:11 +0200

    modified index.js

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 var app = require('express').createServer();
 var sites = require('./lib/sitescollection')();
+
 var port = process.env.PORT || 3000;
 
 // Setup some sites:
-var palabea = sites.createSite({id: "palabea", callbackUrl: "http://google.com/timeout/:userId"});
-var apple   = sites.createSite({id: "apple", callbackUrl: "http://apple.com/timeoutuser/:userId"});
+var palabea = sites.createSite({id: "palabea", callbackUrl: "http://google.com/timeout/:sessionId/:userId"});
 
 
-app.get('/sites/:siteId/users/:userId/ping', function(req,res){
+
+app.get('/sites/:siteId/rooms/:roomId/build', function(req,res){
 
   var site = sites.findOrCreateById(req.params.siteId);
-  var user = site.findOrCreateUserById(req.params.userId);
+  var session = site.findOrCreateSessionById(req.params.sessionId);
+
+  console.log(req.query)
+
+});
+
+app.get('/sites/:siteId/sessions/:sessionId/users/:userId/ping', function(req,res){
+
+  var site = sites.findOrCreateById(req.params.siteId);
+  var session = site.findOrCreateSessionById(req.params.sessionId);
+  var user = session.findOrCreateUserById(req.params.userId);
 
   user.ping();
-
   res.send(user.toJson());
 
 });
