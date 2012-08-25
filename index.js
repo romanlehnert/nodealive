1b499f2c69a7b73167dc9d9a76084d40d975edce
Roman <roman.lehnert@googlemail.com>
2012-08-25 19:15:00 +0200

    added faulty support für session callbacks

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 var app = require('express').createServer();
 var sites = require('./lib/sitescollection')();
+var retry = require('retry');
 
 var port = process.env.PORT || 5000;
 
-sites.addSite({id: "palabea-develop", callbackUrl: "http://localhost:3000/api/durations/:sessionId", api_key: "develop"});
-sites.addSite({id: "palabea-test", callbackUrl: "http://localhost:3001/api/durations/:sessionId", api_key: "test"});
-sites.addSite({id: "palabea-testing", callbackUrl: "http://aebalap:secret!@testing.palabea.com/api/durations/:sessionId", api_key: "testing"});
-sites.addSite({id: "palabea-staging", callbackUrl: "http://staging.palabea.com/api/durations/:sessionId", api_key: "staging"});
-sites.addSite({id: "palabea-production", callbackUrl: "http://palabea.com/api/durations/:sessionId", api_key: "ssecret thing here"});
+var apiKeys = {}
+
+apiKeys["production"] = process.env.api_key_production || "changeme"
+apiKeys["staging"]    = process.env.api_key_staging    || "changeme"
+apiKeys["testing"]    = process.env.api_key_testing    || "testing"
+apiKeys["test"]       = process.env.api_key_test       || "test"
+apiKeys["develop"]    = process.env.api_key_develop    || "develop"
+
+sites.addSite({id: "palabea-develop", callbackUrl: "http://localhost:3000/api/durations/:sessionId", api_key: apiKeys.develop});
+sites.addSite({id: "palabea-test", callbackUrl: "http://localhost:3001/api/durations/:sessionId", api_key: apiKeys.test});
+sites.addSite({id: "palabea-testing", callbackUrl: "http://aebalap:secret!@testing.palabea.com/api/durations/:sessionId", api_key: apiKeys.testing});
+sites.addSite({id: "palabea-staging", callbackUrl: "http://staging.palabea.com/api/durations/:sessionId", api_key: apiKeys.staging });
+sites.addSite({id: "palabea-production", callbackUrl: "http://palabea.com/api/durations/:sessionId", api_key: apiKeys.production});
 
 app.get('/sites/:siteId/sessions/:sessionId/users/:userId/ping', function(req,res){
 
